d5a1c6fabe727b7411e1e89bc1b9e5969eb68211
arshi-shakeel <126773149+arshi-shakeel@users.noreply.github.com>
2023-05-17 19:40:34 +0530

    footer.php

---
+++ student/footer.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
+    <style type ="text/css">
+        footer
+        {
+      height: 200px;
+     width: 1361px;
+     background-color: rgb(10, 20, 49);
+     }   
+     .fa
+     {
+        margin:0px 5px;
+        padding:5px;
+        font-size:20px;
+        width:20px;
+        height:20px;
+        text-align:center;
+        text-decoration:none;
+        border-radius:50%;
+     }
+     .fa:hover
+     {
+        opacity:.7;
+     }
+     .fa-facebook
+     {
+        background:#3B5998;
+        color:white;
+     }
+     .fa-twitter
+     {
+        background:#55ACEE;
+        color:white;
+     }
+     .fa-google
+     {
+        background:#EA4335;
+        color:white;
+     }
+     .fa-instagram
+     {
+        background:#E1306C;
+        color:white;
+     }
+     .fa-youtube
+     {
+        background:#FF0000;
+        color:white;
+     }
+    </style>
+</head>
+<body>
+    <footer style="background-color:rgb(10, 20, 49);">
+    <br>
+    <h3 style="color:white;  text-align: center; ">Contact us through social media</h3><br>
+    <div style="margin:0px 570px;">
+
+    <a href ="#" class="fa fa-facebook"></a>
+    <a href="#"  class="fa fa-twitter"></a>
+    <a href="#" class="fa fa-google"></a>
+    <a href="#" class="fa fa-instagram"></a>
+    <a href="#" class="fa fa-youtube"></a>
+    </div>
+    <br>
+    <p style="color:white;  text-align: center; ">
+				<br>
+				Email: Online.library@gmail.com <br><br>
+				Mobile: +88018*****
+			</p>
+</footer>
+</body>
+</html>
